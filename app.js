706de34d7813f860d926605f3856f44d7b0373fe
DragunovIQ <42098095+DragunovIQ@users.noreply.github.com>
2019-08-14 04:06:09 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var contact = document.getElementById('contact');
 var body = document.getElementById('body');
 var span = document.getElementById('span');
 var tri = document.getElementById('triangle');
+// --------------- Day and night mode -------------
 function night(){
     intro.style.background = 'black';
     body.style.background = 'black';
@@ -12,8 +13,8 @@ function night(){
     span.style.background = 'black';
     span.style.color = '#fbfbfb';
     span.style.transition = '0.5s ease';
-    tri.style.borderRight = 'black';
-    tri.style.borderLeft = 'black';
+    tri.style.borderRight = '85px black solid';
+    tri.style.borderLeft = '85px black solid';
 }
 function light(){
     intro.style.background = '#fbfbfb';
@@ -23,15 +24,36 @@ function light(){
     span.style.background = '#fbfbfb';
     span.style.transition = '0.5s ease';
     span.style.color = 'black';
-    tri.style.borderRight = '#fbfbfb';
-    tri.style.borderLeft = '#fbfbfb';
+    tri.style.borderRight = '85px #fbfbfb solid';
+    tri.style.borderLeft = '85px #fbfbfb solid';
 }
-btn1 = document.getElementById('btn3');
-btn1.addEventListener("mouseover", mouseOver);
-btn1.addEventListener("mouseout", mouseOut);
-function mouseOver(){
-btn1.style.boxShadow = "0px 0px 12px -5px rgba(0,0,0,0.75)";
-}
-function mouseOut(){
-    btn1.style.boxShadow = "none";
-}
+//----------    buttons shadow -------------
+btn1 = document.getElementById('btn1');
+btn2 = document.getElementById('btn2');
+btn3 = document.getElementById('btn3');
+btn4 = document.getElementById('btn4');
+
+btn1.addEventListener('mouseover', ()=> {
+    btn1.style.boxShadow = '0px 0px 12px -5px rgba(0,0,0,0.75)';
+});
+btn1.addEventListener('mouseout', ()=>{
+    btn1.style.boxShadow = 'none';
+});
+btn2.addEventListener('mouseover', ()=>{
+    btn2.style.boxShadow = '0px 0px 12px -5px rgba(0,0,0,0.75)';
+})
+btn2.addEventListener('mouseout', ()=>{
+    btn2.style.boxShadow = 'none';
+});
+btn3.addEventListener('mouseover', ()=>{
+    btn3.style.boxShadow = '0px 0px 12px -5px rgba(0,0,0,0.75)';
+})
+btn3.addEventListener('mouseout', ()=>{
+    btn3.style.boxShadow = 'none';
+});
+btn4.addEventListener('mouseover', ()=>{
+    btn4.style.boxShadow = '0px 0px 12px -5px rgba(0,0,0,0.75)';
+})
+btn4.addEventListener('mouseout', ()=>{
+    btn4.style.boxShadow = 'none';
+});
